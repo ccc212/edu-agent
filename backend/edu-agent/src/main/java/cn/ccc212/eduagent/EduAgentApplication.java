0889547a08c46292354ab9d642dba0274754c9ba
ccc212 <2120496201@qq.com>
2025-06-08 19:18:19 +0800

    初始后端项目

---
+++ backend/edu-agent/src/main/java/cn/ccc212/eduagent/EduAgentApplication.java
@@ -0,0 +1,14 @@
+package cn.ccc212.eduagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAgentApplication.class, args);
+		System.out.println("启动成功");
+	}
+
+}
