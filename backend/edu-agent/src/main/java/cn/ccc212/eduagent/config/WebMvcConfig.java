75ddee367b72b3a446d6d0183a3c60189c70ceda
ccc212 <2120496201@qq.com>
2025-06-13 21:13:05 +0800

    fix(doc): 解决添加了消息转换器导致 knife4j 不能访问的问题

--- backend/edu-agent/src/main/java/cn/ccc212/eduagent/config/WebMvcConfig.java
+++ backend/edu-agent/src/main/java/cn/ccc212/eduagent/config/WebMvcConfig.java
@@ -65,7 +65,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //需要为消息转换器设置一个对象转换器，对象转换器可以将Java对象序列化为json数据
         converter.setObjectMapper(new JacksonObjectMapper());
         //将自己的信息转换器加入容器中
-        converters.add(0, converter);
+        converters.add(1, converter);
     }
 
 }
