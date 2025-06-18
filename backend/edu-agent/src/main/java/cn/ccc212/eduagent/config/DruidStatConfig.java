95901b77d6f93a9af246a25e7cbe8e7a39a0d215
ccc212 <2120496201@qq.com>
2025-06-18 10:39:29 +0800

    feat(druid): 添加 Druid 监控配置

---
+++ backend/edu-agent/src/main/java/cn/ccc212/eduagent/config/DruidStatConfig.java
@@ -0,0 +1,21 @@
+package cn.ccc212.eduagent.config;
+
+import com.alibaba.druid.support.jakarta.StatViewServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DruidStatConfig {
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        servletRegistrationBean.addInitParameter("allow", "");
+        servletRegistrationBean.addInitParameter("deny", "");
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+}
